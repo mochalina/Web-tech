cb4cd202fdb7cb5dc29da4e27def6249fcb7a54a
mochalina <moch_alina@mail.ru>
2022-04-20 23:03:40 +0500

    2 laba

---
+++ 2/index.php
@@ -0,0 +1,18 @@
+<html>
+<head> <title> Лаба2 </title> </head>
+<div align="left">
+  <a  href="http://a0657623.xsph.ru/index.php">Вернуться на главную </a>  
+</div>
+<body align="center">
+<h1>Лабораторная работа №2</h1><p>
+<h2>Мочалина Алина Сергеевна ПИ-323</h2><p>
+<u1>
+        <li><a href="http://a0657623.xsph.ru/22/2-1.html">Задание 1. </a></li>
+        <li><a href="http://a0657623.xsph.ru/22/2-2.html">Задание 2. </a></li>
+        <li><a href="http://a0657623.xsph.ru/22/2-3.html">Задание 3. </a></li>
+        <li><a href="http://a0657623.xsph.ru/22/2-4.html">Задание 4. </a></li>
+        <li><a href="http://a0657623.xsph.ru/22/2-5.html">Задание 5. </a></li>
+        <li><a href="http://a0657623.xsph.ru/22/2-6.html">Задание 6. </a></li>
+        <li><a href="http://a0657623.xsph.ru/22/2-7.html">Задание 7. </a></li>
+</u1>
+</html>
